13ac3f6762296edf7f0135700142e26d64b22e49
Kirill Priyatnov <kirill.priyatnov@gmail.com>
2022-02-05 13:16:22 +0300

    echo loop added in EchoServer

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -5,29 +5,33 @@ import java.util.Scanner;
 
 public class EchoServer {
 
+
     public static void main(String[] args) throws IOException {
         //создаем серверный сокер на порту 1234
         ServerSocket server = new ServerSocket(1234);
-        System.out.println("Waiting");
+        while (true){
+            System.out.println("Waiting");
 
-        //ждем клиента
-        Socket s = server.accept();
-        System.out.println("Client connected!");
+            //ждем клиента
+            Socket s = server.accept();
+            System.out.println("Client connected!");
 
-        //получаем потоки ввода и вывода
-        InputStream is = s.getInputStream();
-        OutputStream os = s.getOutputStream();
+            //получаем потоки ввода и вывода
+            InputStream is = s.getInputStream();
+            OutputStream os = s.getOutputStream();
 
-        //Создаем удобные средства ввода и вывода
-        Scanner in = new Scanner(is);
-        PrintStream out = new PrintStream(os);
+            //Создаем удобные средства ввода и вывода
+            Scanner in = new Scanner(is);
+            PrintStream out = new PrintStream(os);
 
-        //читаем из сети и пишем в сеть
-        out.println("Welcome, friend!");
-        String input = in.nextLine();
-        while (!(input.equals("bye"))) {
-            out.println(input + "-" + input + "-" + input.substring(input.length()/2) + "...");
-            input = in.nextLine();
+            //читаем из сети и пишем в сеть
+            out.println("Welcome, friend!");
+            String input = in.nextLine();
+            while (!(input.equals("bye"))) {
+                out.println(input + "-" + input + "-" + input.substring(input.length()/2) + "...");
+                input = in.nextLine();
+            }
+            s.close();
         }
     }
 }
