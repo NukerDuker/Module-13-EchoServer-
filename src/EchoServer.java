1b1d38435db0a1d3e7e9acd4980bec5e07f75417
Kirill Priyatnov <kirill.priyatnov@gmail.com>
2022-02-05 12:50:25 +0300

    simple server

---
+++ src/EchoServer.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServer {
+
+    public static void main(String[] args) throws IOException {
+        //создаем серверный сокер на порту 1234
+        ServerSocket server = new ServerSocket(1234);
+        System.out.println("Waiting");
+
+        //ждем клиента
+        Socket s = server.accept();
+        System.out.println("Client connected!");
+
+        //получаем потоки ввода и вывода
+        InputStream is = s.getInputStream();
+        OutputStream os = s.getOutputStream();
+
+        //Создаем удобные средства ввода и вывода
+        Scanner in = new Scanner(is);
+        PrintStream out = new PrintStream(os);
+
+        //читаем из сети и пишем в сеть
+        out.println("What`s your name?");
+        out.println("Hello, " + in.nextLine());
+    }
+}
